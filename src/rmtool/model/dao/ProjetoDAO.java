d11d50d2a030155376f1c9b2b6161f8361bcd2bd
hrsantos <haroldo.rsantos@gmail.com>
2014-05-05 20:46:27 -0300

    Realizado CRUD ProjetoDAO

--- src/rmtool/model/dao/ProjetoDAO.java
+++ src/rmtool/model/dao/ProjetoDAO.java
@@ -6,10 +6,67 @@
 
 package rmtool.model.dao;
 
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import rmtool.model.bean.Projeto;
+
 /**
  *
  * @author jonimane
  */
 public class ProjetoDAO {
+    public void criar( Projeto p ) throws Exception
+    {
+        Session s;
+        
+        s = HibernateUtilDAO.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        s.save(p);
+        s.getTransaction().commit();
+    }
     
+    public void alterar( Projeto p ) throws Exception
+    {
+        Session s;
+        
+        s = HibernateUtilDAO.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        s.merge(p);
+        s.getTransaction().commit();
+    }
+    
+    public void excluir( Projeto p ) throws Exception
+    {
+        Session s;
+        
+        s = HibernateUtilDAO.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        s.delete(p);
+        s.getTransaction().commit();
+    }
+    
+    public List<Projeto> listar() throws Exception
+    {
+        Session s;
+        
+        s = HibernateUtilDAO.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        List<Projeto> requisitos = s.createCriteria(Projeto.class).list();
+        
+        return requisitos;
+    }
+    
+    public Projeto procurarId(int id) throws Exception
+    {
+        Session s;
+        
+        s = HibernateUtilDAO.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        Criteria cri = s.createCriteria(Projeto.class);
+        cri.add(Restrictions.eq("id", id));
+        
+        return (Projeto) cri.list().get(0);
+    }
 }
